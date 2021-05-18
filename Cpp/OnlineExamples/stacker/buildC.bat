50f1e55c57c026722973cfb04697310220e667e1
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-05-18 17:18:36 -0500

    unnecesary files removed

--- Cpp/OnlineExamples/stacker/buildC.bat
+++ Cpp/OnlineExamples/stacker/buildC.bat
@@ -1,1 +1,1 @@
-call emcc stacker.c -s USE_SDL=2 --preload-file Resources -s USE_SDL_IMAGE=2 -s ALLOW_MEMORY_GROWTH=1 --use-preload-plugins -s SDL2_IMAGE_FORMATS='["bmp"]' -o stacker.html
+call emcc stacker.c -s USE_SDL=2 --preload-file Resources -s USE_SDL_IMAGE=2 --use-preload-plugins -s SDL2_IMAGE_FORMATS='["bmp"]' -o stacker.html
